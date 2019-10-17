45ba8d44b8cf64d6f07f50d5fc60c18a83e985d9
Tharushan <tharushan.saseeskumar@gmail.com>
2019-10-17 16:13:31 +0200

    Add: tests

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const v1 = require('./src/routes/v1/players');
 const v2 = require('./src/routes/v2/players');
 
 app.use('/api/v1/players', v1);
-app.use('/api/v2/players', v2);
+app.use('/players', v2);
 app.listen(port, host, () => {
   console.log('App listening on %s:%i', host, port);
 });
