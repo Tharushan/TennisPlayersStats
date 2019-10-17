fd4de40795d2b39731236dfd9afff8161864b726
Tharushan <Tharushan@users.noreply.github.com>
2019-10-17 17:00:45 +0200

    Fix: env variable port and host

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const app = require('express')();
 const { port, host } = require('config');
+
+const PORT = process.env.PORT || port;
+const HOST = process.env.HOST || host;
 const v1 = require('./src/routes/v1/players');
 const v2 = require('./src/routes/v2/players');
 
 app.use('/api/v1/players', v1);
 app.use('/players', v2);
-app.listen(port, host, () => {
-  console.log('App listening on %s:%i', host, port);
+app.listen(PORT, HOST, () => {
+  console.log('App listening on %s:%i', HOST, PORT);
 });
