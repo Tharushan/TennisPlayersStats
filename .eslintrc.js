6ed42f97670f1fd16522ef35ce528ea8ed17de0c
Tharushan <Tharushan@users.noreply.github.com>
2019-10-17 13:23:43 +0200

    Add: project start

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  extends: ['airbnb', 'prettier'],
+  env: {
+    node: true,
+    es6: true,
+    mocha: true
+  },
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        trailingComma: 'none',
+        bracketSpacing: true,
+        jsxBracketSameLine: false,
+        parser: 'babylon'
+      }
+    ],
+    'func-names': ['off'],
+    'no-underscore-dangle': ['off'],
+    'class-methods-use-this': ['off'],
+    'no-console': 'off'
+  }
+};
