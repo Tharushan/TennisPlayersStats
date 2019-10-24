89d30fb86fc943900002a539d57cf0a169c757ae
Tharushan <tharushan.saseeskumar@gmail.com>
2019-10-24 13:49:56 +0200

    Remove: useless describe layer + fix async/await supertest

--- test/routes/fromApi/playersTest.js
+++ test/routes/fromApi/playersTest.js
@@ -3,68 +3,46 @@ const request = require('supertest');
 const app = require('../../../index');
 const PlayerController = require('../../../src/controllers/PlayerController');
 
-describe('Unit tests', () => {
-  describe('API routes', () => {
-    let getPlayersFromAPI;
-    beforeEach(() => {
-      getPlayersFromAPI = sinon
-        .stub(PlayerController, 'getPlayersFromAPI')
-        .returns([{ id: 1 }, { id: 5 }]);
+describe('API routes', () => {
+  let getPlayersFromAPI;
+  beforeEach(() => {
+    getPlayersFromAPI = sinon
+      .stub(PlayerController, 'getPlayersFromAPI')
+      .returns([{ id: 1 }, { id: 5 }]);
+  });
+
+  afterEach(() => {
+    getPlayersFromAPI.restore();
+  });
+  describe('List /players/ route', () => {
+    it('should return 200', async () => {
+      const res = await request(app).get('/players');
+      res.statusCode.should.eql(200);
+      res.body.should.be.an.Array();
     });
 
-    afterEach(() => {
-      getPlayersFromAPI.restore();
+    it('should return 500', async () => {
+      getPlayersFromAPI.throws('Unexpected error');
+      const res = await request(app).get('/players');
+      res.statusCode.should.eql(500);
     });
-    describe('List /players/ route', () => {
-      it('should return 200', async () => {
-        await request(app)
-          .get('/players')
-          .end((err, res) => {
-            if (err) throw err;
-            res.statusCode.should.eql(200);
-            res.body.should.be.an.Array();
-          });
-      });
-
-      it('should return 500', async () => {
-        getPlayersFromAPI.throws('Unexpected error');
-        await request(app)
-          .get('/players')
-          .end((err, res) => {
-            if (err) throw err;
-            res.statusCode.should.eql(500);
-          });
-      });
+  });
+  describe('Get /players/:id route', () => {
+    it('should return 404', async () => {
+      const res = await request(app).get('/players/34334');
+      res.statusCode.should.eql(404);
     });
-    describe('Get /players/:id route', () => {
-      it('should return 404', async () => {
-        await request(app)
-          .get('/players/34334')
-          .end((err, res) => {
-            if (err) throw err;
-            res.statusCode.should.eql(404);
-          });
-      });
 
-      it('should return 200', async () => {
-        await request(app)
-          .get('/players/5')
-          .end((err, res) => {
-            if (err) throw err;
-            res.statusCode.should.eql(200);
-            res.body.id.should.eql(5);
-          });
-      });
+    it('should return 200', async () => {
+      const res = await request(app).get('/players/5');
+      res.statusCode.should.eql(200);
+      res.body.id.should.eql(5);
+    });
 
-      it('should return 500', async () => {
-        getPlayersFromAPI.throws('Unexpected error');
-        await request(app)
-          .get('/players/5')
-          .end((err, res) => {
-            if (err) throw err;
-            res.statusCode.should.eql(500);
-          });
-      });
+    it('should return 500', async () => {
+      getPlayersFromAPI.throws('Unexpected error');
+      const res = await request(app).get('/players/5');
+      res.statusCode.should.eql(500);
     });
   });
 });
